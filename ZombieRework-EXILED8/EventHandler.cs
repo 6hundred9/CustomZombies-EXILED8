ecacf3ddecb8b6408d90691e3571a6d9c2630d9b
Mert Ali Bayram <mertali.bayram01@dogaokullari.k12.tr>
2024-04-08 15:52:24 +0300

    Complete rework on spawn system

---
+++ ZombieRework-EXILED8/EventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Exiled.Events.EventArgs.Scp049;
+// ReSharper disable IdentifierTypo
+
+namespace ZombieRework_EXILED8
+{
+    public class EventHandler
+    {
+        public static void RecallFinished(FinishingRecallEventArgs ev)
+        {
+            int asyphixator = UnityEngine.Random.Range(1, 2);
+            int speedjunkie = UnityEngine.Random.Range(1, 4);
+            int tanky = UnityEngine.Random.Range(3,4);
+            int subject = UnityEngine.Random.Range(3, 10);
+            if (asyphixator == 1)
+            {
+                 Plugin.Instance.Config.Asyphixator.AddRole(ev.Player);
+            } else if (speedjunkie == 1)
+            {
+                Plugin.Instance.Config.Speedjunkie.AddRole(ev.Player);
+            } else if (tanky == 3)
+            {
+                Plugin.Instance.Config.Tanky.AddRole(ev.Player);
+            } else if (subject == 3)
+            {
+                Plugin.Instance.Config.TestSubject.AddRole(ev.Player);
+            }
+        }
+    }
+}
--- ZombieRework-EXILED8/Zombies/Asyphixator.cs
+++ ZombieRework-EXILED8/Zombies/Asyphixator.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections.Generic;
-using Exiled.API.Enums;
-using Exiled.API.Features.Spawn;
+﻿using Exiled.API.Enums;
 using Exiled.CustomRoles.API.Features;
 using Exiled.Events.EventArgs.Player;
-using Exiled.Events.Handlers;
 using PlayerRoles;
+using Player = Exiled.API.Features.Player;
+
 
 namespace ZombieRework_EXILED8.Zombies
 {
@@ -13,33 +12,31 @@ namespace ZombieRework_EXILED8.Zombies
         public override uint Id { get; set; } = 1;
         public override RoleTypeId Role { get; set; } = RoleTypeId.Scp0492;
         public override int MaxHealth { get; set; } = 600;
-        public override string Name { get; set; } = "Asyphixator";
+        public override string Name { get; set; } = "SCP-049-2-Asyphixator";
         public override string Description { get; set; } = "Suffocate your enemies to death";
         public override string CustomInfo { get; set; } = "Asyphixator";
         public override bool KeepPositionOnSpawn { get; set; } = true;
         
-        public override SpawnProperties SpawnProperties { get; set; } = new()
+        //1,2
+        public override void AddRole(Player player)
         {
-            RoleSpawnPoints = new List<RoleSpawnPoint>
-            {
-                new()
-                {
-                    Role = RoleTypeId.Scp0492,
-                    Chance = 50
-                }
-            }
-        };
-        
+            player.Role.Set(RoleTypeId.Scp0492);
+            SubscribeEvents();
+            player.RankName = Name;
+            player.Health = MaxHealth;
+            player.CustomInfo = CustomInfo;
+            base.AddRole(player);
+        }
 
         protected override void SubscribeEvents()
         {
-            Player.Hurting += OnHurting;
+            Exiled.Events.Handlers.Player.Hurting += OnHurting;
             base.SubscribeEvents();
         }
 
         protected override void UnsubscribeEvents()
         {
-            Player.Hurting -= OnHurting;
+            Exiled.Events.Handlers.Player.Hurting -= OnHurting;
             base.UnsubscribeEvents();
         }
 
